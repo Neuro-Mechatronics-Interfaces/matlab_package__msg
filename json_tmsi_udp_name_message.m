7afb6588fdfd46ee7e9d1dd80f003f26a7aa5cba
Max Murphy <max.murphy11@gmail.com>
2023-03-14 13:10:10 -0400

    Add more input handling for json_tmsi_udp_name_message

--- json_tmsi_udp_name_message.m
+++ json_tmsi_udp_name_message.m
@@ -2,7 +2,12 @@ function data = json_tmsi_udp_name_message(SUBJ, YYYY, MM, DD, ARRAY, BLOCK)
 %JSON_TMSI_UDP_NAME_MESSAGE  Generate TMSi-filename data for UDP/JSON message.
 %
 % Syntax:
+%   data = msg.json_tmsi_udp_name_message();
+%       -> This returns the default message structure.
+%   data = msg.json_tmsi_udp_name_message(SUBJ, YYYY, MM, DD, BLOCK);
+%       -> Automatically uses "%s" for ARRAY value.
 %   data = msg.json_tmsi_udp_name_message(SUBJ, YYYY, MM, DD, ARRAY, BLOCK);
+%       -> Standard syntax.
 %
 % Inputs:
 %   parameter - Name of the parameter to control 
@@ -23,6 +28,13 @@ function data = json_tmsi_udp_name_message(SUBJ, YYYY, MM, DD, ARRAY, BLOCK)
 % See also: Contents
 if nargin < 1
     data = struct('type', 'name.tmsi', 'subject', "Forrest", 'year', 2023, 'month', 3, 'day', 12, 'array', "A", 'block', 0);
+elseif nargin < 6
+    if ~isnumeric(ARRAY)
+        error("Expected 5th argument to be BLOCK if only 5 arguments are given. Should be a scalar numeric value, but instead it was %s.", ARRAY);
+    end
+    BLOCK = ARRAY;
+    ARRAY = "%s";
+    data = struct('type', 'name.tmsi', 'subject', SUBJ, 'year', YYYY, 'month', MM, 'day', DD, 'array', ARRAY, 'block', BLOCK);
 else
     data = struct('type', 'name.tmsi', 'subject', SUBJ, 'year', YYYY, 'month', MM, 'day', DD, 'array', ARRAY, 'block', BLOCK);
 end
